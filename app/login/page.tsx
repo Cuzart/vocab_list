c9a3e70c91484e0ee604dd44c98e6bf0360009dc
Chris Heinz <chrisi.heinz@web.de>
2023-11-25 20:38:16 +0100

    make button submit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-import { headers, cookies } from 'next/headers';
+import { cookies } from 'next/headers';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import {
@@ -33,29 +33,29 @@ export default async function Login({ searchParams }: { searchParams: { message:
     return redirect('/');
   };
 
-  const signUp = async (formData: FormData) => {
-    'use server';
+  // const signUp = async (formData: FormData) => {
+  //   'use server';
 
-    const origin = headers().get('origin');
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
+  //   const origin = headers().get('origin');
+  //   const email = formData.get('email') as string;
+  //   const password = formData.get('password') as string;
+  //   const cookieStore = cookies();
+  //   const supabase = createClient(cookieStore);
 
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
+  //   const { error } = await supabase.auth.signUp({
+  //     email,
+  //     password,
+  //     options: {
+  //       emailRedirectTo: `${origin}/auth/callback`,
+  //     },
+  //   });
 
-    if (error) {
-      return redirect('/login?message=Could not authenticate user');
-    }
+  //   if (error) {
+  //     return redirect('/login?message=Could not authenticate user');
+  //   }
 
-    return redirect('/login?message=Check email to continue sign in process');
-  };
+  //   return redirect('/login?message=Check email to continue sign in process');
+  // };
 
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
@@ -78,7 +78,9 @@ export default async function Login({ searchParams }: { searchParams: { message:
             <PasswordInput mb={30} label='Passwort' name='password' required />
 
             {/* <Button formAction={signUp}>Sign Up</Button> */}
-            <Button fullWidth>Sign In</Button>
+            <Button type='submit' fullWidth>
+              Sign In
+            </Button>
             {searchParams?.message && <Text>{searchParams.message}</Text>}
           </Box>
         </Center>
